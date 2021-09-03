3f34d6b79ee53969b1cbd43684f03eb9d0a37e49
josh manansala <joshmanansalagsd@gmail.com>
2021-09-02 17:52:56 -0700

    Responsive navbar mockup with vanilla js

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# demo
+# Responsive Navbar Practive
+## HTML CSS JS vanilla 
+
+### Subjects and Concepts to work on
+- Events
+- Display
+- Media Queries
+- Position
+- Display
+- CSS Foundations 
---
+++ script.js
@@ -0,0 +1,20 @@
+// Contained elements 
+const navIcon = document.querySelector(".icon");
+
+/* Toggle between adding and removingthe "responsive" class to topnav when the
+    user clicks on the icon
+ */
+
+function topNavClassToggle() {
+    let myTopNav = document.getElementById("myTopNav");
+    
+    if (myTopNav.className === "topnav") {
+        myTopNav.className += " responsive";
+    } else {
+        myTopNav.className = "topnav";
+    }
+}
+
+// Waits for user click to run function (topNavClassToggle)
+navIcon.addEventListener('click', topNavClassToggle);
+
